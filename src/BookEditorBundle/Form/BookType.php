62dd2e43f317ce10ea7aee44655dd3685f9988a7
ngdo-pro <ngdo27@gmail.com>
2016-10-11 07:54:15 +0200

    Book crud generated

---
+++ src/BookEditorBundle/Form/BookType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace BookEditorBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class BookType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('author')
+            ->add('description')
+            ->add('facebookLinkUrl')
+            ->add('imageUrl')
+            ->add('pressTitle')
+            ->add('pressImageUrl')
+            ->add('releaseDate', 'datetime')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'BookEditorBundle\Entity\Book'
+        ));
+    }
+}
