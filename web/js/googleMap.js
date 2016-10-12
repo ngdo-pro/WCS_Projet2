6f1279b946618ca2a1763699995da71b439a05a0
christianJCK <jecko.christian@gmail.com>
2016-10-12 18:04:00 +0200

    embed map

--- web/js/googleMap.js
+++ web/js/googleMap.js
@@ -1,13 +1,2 @@
-function initMap() {
-    var lyon = {lat: 45.763859, lng: 4.880444};
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 16,
-        center: lyon
-    });
-    var marker = new google.maps.Marker({
-        position: lyon,
-        map: map
-    });
-}
 
 
