b7d979e0bbd7535f0bc175437c2c275cfa900308
gianni-n <gl.napoli82@gmail.com>
2016-10-12 17:16:11 +0200

    pushing block book

--- src/BookEditorBundle/Controller/BookController.php
+++ src/BookEditorBundle/Controller/BookController.php
@@ -155,4 +155,5 @@ class BookController extends Controller
     }
 
 
+
 }
--- src/BookEditorBundle/Controller/DefaultController.php
+++ src/BookEditorBundle/Controller/DefaultController.php
@@ -10,17 +10,7 @@ class DefaultController extends Controller
     {
         return $this->render('BookEditorBundle:Default:index.html.twig');
     }
-    function cutString($description) {
-        $length = 50;
-        if(strlen($description) > $length) {
-            $cut = substr($description, 0, $length) . '...';
-            return $cut;
-        }
-        else{
-            return $description;
-        }
 
-    }
 
 
 
