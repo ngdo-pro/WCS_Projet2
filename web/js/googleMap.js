ca82be9bfe3caf36e7f59709ecbe34752ce7afab
christianJCK <jecko.christian@gmail.com>
2016-10-12 15:57:53 +0200

    google map integration

---
+++ web/js/googleMap.js
@@ -0,0 +1,8 @@
+/**
+ * Created by apprenti on 12/10/16.
+ */
+function initMap() {
+    var uluru = {lat :  -25.363, lng: 131.044};
+    var map = new google.maps.Map(document.getElementById('map'), {
+        }
+        </script>
