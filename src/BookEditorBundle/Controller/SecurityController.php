c838de59918f2c9fc673c5a871139c7926f5641e
Nagx <k.javazzo@gmail.com>
2016-10-25 17:32:43 +0200

    authentication ok with redirection

--- src/BookEditorBundle/Controller/SecurityController.php
+++ src/BookEditorBundle/Controller/SecurityController.php
@@ -9,19 +9,17 @@ class SecurityController extends Controller
 {
     public function loginAction(Request $request)
     {
-        // Si le visiteur est déjà identifié, on le redirige vers l'accueil
-        if ($this->get('security.authorization_checker')->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
-            return $this->redirectToRoute('oc_platform_accueil');
-        }
-
-        // Le service authentication_utils permet de récupérer le nom d'utilisateur
-        // et l'erreur dans le cas où le formulaire a déjà été soumis mais était invalide
-        // (mauvais mot de passe par exemple)
         $authenticationUtils = $this->get('security.authentication_utils');
 
-        return $this->render('BookEditorBundle:Security:login.html.twig', array(
-            'last_username' => $authenticationUtils->getLastUsername(),
-            'error'         => $authenticationUtils->getLastAuthenticationError(),
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', array(
+            'last_username' => $lastUsername,
+            'error'         => $error,
         ));
     }
 }
