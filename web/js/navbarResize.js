b8e2b734f180f305117e68d7b7a56fb23e8483cc
christianJCK <jecko.christian@gmail.com>
2016-10-26 16:19:28 +0200

    solved background clipping

--- web/js/navbarResize.js
+++ web/js/navbarResize.js
@@ -1,8 +1,7 @@
 $(function(){
     var offset = $('#content').css('padding-top');
-    offset = offset.substring(0, offset.length-2);
     $(window).scroll(function(){
-        if ($(document).scrollTop() > offset) {
+        if ($(document).scrollTop() > 125) {
             $('.navbar-brand img').addClass('resizeImg');
             $('.navbar-default').addClass('resizeBackground');
             $('.navbar-default .navbar-nav > li > a').addClass('resizeLink');
