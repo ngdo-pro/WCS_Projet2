752afa8686e5c571bc4280e5cbc7bbf98a636cb1
ngdo-pro <ngdo27@gmail.com>
2016-10-26 13:13:20 +0200

    created PressArticle Entity

---
+++ src/BookEditorBundle/Entity/PressArticle.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace BookEditorBundle\Entity;
+
+/**
+ * PressArticle
+ */
+class PressArticle
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $title;
+
+    /**
+     * @var string
+     */
+    private $imageUrl;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return PressArticle
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set imageUrl
+     *
+     * @param string $imageUrl
+     *
+     * @return PressArticle
+     */
+    public function setImageUrl($imageUrl)
+    {
+        $this->imageUrl = $imageUrl;
+
+        return $this;
+    }
+
+    /**
+     * Get imageUrl
+     *
+     * @return string
+     */
+    public function getImageUrl()
+    {
+        return $this->imageUrl;
+    }
+}
+
